bf535c12b9866ed51465c32fb0bcf41bd8190ec0
Sohaib kabeer <sohaibkabeer@gmail.com>
2026-03-04 01:21:31 +0500

    LightRays added to the parent layout
    LightRays added to the parent layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import LightRays from "@/components/LightRays";
 
 const SchibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -27,7 +28,22 @@ export default function RootLayout({
       <body
         className={`${SchibstedGrotesk.variable} ${MartianMono.variable} min-h-screen antialiased`}
       >
+      <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={1}
+              lightSpread={0.5}
+              rayLength={3}
+              followMouse={true}
+              mouseInfluence={0.1}
+              noiseAmount={0}
+              distortion={0}
+          />
+      </div>
+      <main>
         {children}
+      </main>
       </body>
     </html>
   );
